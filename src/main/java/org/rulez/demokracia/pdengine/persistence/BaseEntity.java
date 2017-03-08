bd9d90d9693c15638ce3d914d4fbb6150d7550fb
Mag <mag@magwas.rulez.org>
2017-03-08 18:55:34 +0100

    removed unused code

--- src/main/java/org/rulez/demokracia/pdengine/persistence/BaseEntity.java
+++ src/main/java/org/rulez/demokracia/pdengine/persistence/BaseEntity.java
@@ -1,9 +1,7 @@
 package org.rulez.demokracia.pdengine.persistence;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
-import javax.persistence.Column;
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
@@ -11,57 +9,18 @@ import javax.persistence.InheritanceType;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class BaseEntity implements IEntity {
+public abstract class BaseEntity implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	public String id;
 	
-	@Column(name = "Deleted")
-	private Boolean _deleted;
-	
-	private Boolean _dirty;
-	
-    private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
-
     public BaseEntity() {
-    	
-    	propertyChangeSupport.addPropertyChangeListener(this);
     }
 	
 	public String getId() {
 		return id;
 	}
 
-	public Boolean getDeleted() {
-		return _deleted;
-	}
-
-	public void setDeleted(Boolean deleted) {
-		if(!_deleted.equals(deleted)) {
-			propertyChangeSupport.firePropertyChange("Deleted", deleted, _deleted);
-			_deleted = deleted;
-		}
-	}
-
-	public Boolean getDirty() {
-		return _dirty;
-	}
-
-	public void propertyChange(PropertyChangeEvent event) {
-		_dirty = true;
-	}
-	
-	public void addPropertyChangeListener(PropertyChangeListener listener) {
-		if(listener == null) throw new NullPointerException("listener");
-		
-		propertyChangeSupport.addPropertyChangeListener(listener);
-	}
-
-	public void removePropertyChangeListener(PropertyChangeListener listener) {
-		if(listener == null) throw new NullPointerException("listener");
-
-		propertyChangeSupport.removePropertyChangeListener(listener);
-	}
 }
