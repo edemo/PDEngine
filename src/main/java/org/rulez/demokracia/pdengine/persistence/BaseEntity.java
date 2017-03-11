37648f213e60a22b733ac26a9c8b3e1dc39f5b95
Mag <mag@magwas.rulez.org>
2017-03-11 09:53:45 +0100

    maven war plugin version

--- src/main/java/org/rulez/demokracia/pdengine/persistence/BaseEntity.java
+++ src/main/java/org/rulez/demokracia/pdengine/persistence/BaseEntity.java
@@ -11,7 +11,7 @@ import org.rulez.demokracia.pdengine.RandomUtils;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class BaseEntity implements Serializable{
+public abstract class BaseEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
