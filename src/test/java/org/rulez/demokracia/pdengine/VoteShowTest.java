d49160832aa7fead89c74ad3820d24a7f017a349
Bujdoso Valentin <valentin.alex.bujdoso@otpbank.hu>
2019-03-12 14:40:51 +0100

    Fix

---
+++ src/test/java/org/rulez/demokracia/pdengine/VoteShowTest.java
@@ -0,0 +1,147 @@
+package org.rulez.demokracia.pdengine;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.rulez.demokracia.pdengine.annotations.tested_behaviour;
+import org.rulez.demokracia.pdengine.annotations.tested_feature;
+import org.rulez.demokracia.pdengine.annotations.tested_operation;
+import org.rulez.demokracia.pdengine.exception.ReportedException;
+import org.rulez.demokracia.pdengine.testhelpers.CreatedDefaultVoteRegistry;
+
+public class VoteShowTest extends CreatedDefaultVoteRegistry {
+
+	Vote vote;
+	String voteId;
+	String adminKey;
+	
+	public void setUp() throws ReportedException {
+		super.setUp();
+		vote = voteManager.getVote(adminInfo.voteId);
+		voteId = adminInfo.voteId;
+		adminKey = adminInfo.adminKey;
+	}
+	
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("returns the vote in json")
+	@Test
+	public void the_name_attribute_contains_the_name_of_the_vote() throws ReportedException {
+		JSONObject result = voteManager.showVote(voteId, adminKey);
+		
+		assertEquals(result.get("name"), vote.name);
+	}
+	
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("returns the vote in json")
+	@Test
+	public void the_canAddIn_attribute_contains_the_addability_of_the_vote() throws ReportedException {
+		JSONObject result = voteManager.showVote(voteId, adminKey);
+		
+		assertEquals(result.get("canAddIn"), vote.canAddin);
+	}
+
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("returns the vote in json")
+	@Test
+	public void the_creationTime_attribute_contains_the_creation_time_of_the_vote() throws ReportedException {
+		JSONObject result = voteManager.showVote(voteId, adminKey);
+		
+		assertEquals(result.get("creationTime"), vote.creationTime);
+	}
+	
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("returns the vote in json")
+	@Test
+	public void the_choices_attribute_contains_the_choices_of_the_vote() throws ReportedException {
+		JSONObject result = voteManager.showVote(voteId, adminKey);
+		JSONObject jsonObject = new JSONObject(vote.choices);
+
+		assertEquals(result.get("choices").toString(), jsonObject.toString());
+	}
+	
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("returns the vote in json")
+	@Test
+	public void the_canEndorse_attribute_contains_the_endorsable_of_the_vote() throws ReportedException {
+		JSONObject result = voteManager.showVote(voteId, adminKey);
+		
+		assertEquals(result.get("canEndorse"), vote.canEndorse);
+	}
+	
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("returns the vote in json")
+	@Test
+	public void the_countedAssurances_attribute_contains_the_counted_assurances_of_the_vote() throws ReportedException {
+		JSONObject result = voteManager.showVote(voteId, adminKey);
+		JSONArray jsonArray = new JSONArray(vote.countedAssurances);
+		
+		assertEquals(result.get("countedAssurances").toString(), jsonArray.toString());
+	}
+	
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("returns the vote in json")
+	@Test
+	public void the_neededAssurances_attribute_contains_the_needed_assurances_of_the_vote() throws ReportedException {
+		JSONObject result = voteManager.showVote(voteId, adminKey);
+		JSONArray jsonArray = new JSONArray(vote.neededAssurances);
+		
+		
+		assertEquals(result.get("neededAssurances").toString(), jsonArray.toString());
+	}
+	
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("returns the vote in json")
+	@Test
+	public void the_minEndorsements_attribute_contains_the_mininimum_endorsements_of_the_vote() throws ReportedException {
+		JSONObject result = voteManager.showVote(voteId, adminKey);
+		
+		assertEquals(result.get("minEndorsements"), vote.minEndorsements);
+	}
+	
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("returns the vote in json")
+	@Test
+	public void the_id_attribute_contains_the_id_of_the_vote() throws ReportedException {
+		JSONObject result = voteManager.showVote(voteId, adminKey);
+		
+		assertEquals(result.get("id"), vote.id);
+	}
+	
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("returns the vote in json")
+	@Test
+	public void the_canView_attribute_contains_the_viewability_of_the_vote() throws ReportedException {
+		JSONObject result = voteManager.showVote(voteId, adminKey);
+		
+		assertEquals(result.get("canView"), vote.canView);
+	}
+	
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("returns the vote in json")
+	@Test
+	public void the_canVote_attribute_contains_the_voteability_of_the_vote() throws ReportedException {
+		JSONObject result = voteManager.showVote(voteId, adminKey);
+		
+		assertEquals(result.get("canVote"), vote.canVote);
+	}
+	
+}
--- src/test/java/org/rulez/demokracia/pdengine/VoteShowValidationTest.java
+++ src/test/java/org/rulez/demokracia/pdengine/VoteShowValidationTest.java
@@ -1,15 +1,5 @@
 package org.rulez.demokracia.pdengine;
 
-import static org.junit.Assert.assertEquals;
-
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.junit.Test;
 import org.rulez.demokracia.pdengine.annotations.tested_behaviour;
 import org.rulez.demokracia.pdengine.annotations.tested_feature;
@@ -19,15 +9,8 @@ import org.rulez.demokracia.pdengine.testhelpers.CreatedDefaultVoteRegistry;
 
 public class VoteShowValidationTest extends CreatedDefaultVoteRegistry {
 
-	Vote vote;
-	String voteId;
-	String adminKey;
-	
-	public void setUp() throws ReportedException {
-		super.setUp();
-		vote = voteManager.getVote(adminInfo.voteId);
-		voteId = adminInfo.voteId;
-		adminKey = adminInfo.adminKey;
+	public VoteShowValidationTest() {
+		super();
 	}
 
 	@tested_feature("Manage votes")
@@ -51,119 +34,4 @@ public class VoteShowValidationTest extends CreatedDefaultVoteRegistry {
 			() -> voteManager.showVote(adminInfo.voteId, invalidAdminKey)
 		).assertMessageIs(String.format("Illegal adminKey: %s",invalidAdminKey));
 	}
-	
-	@tested_feature("Manage votes")
-	@tested_operation("show vote")
-	@tested_behaviour("returns the vote in json")
-	@Test
-	public void check_name() throws ReportedException {
-		JSONObject result = voteManager.showVote(voteId, adminKey);
-		
-		assertEquals(result.get("name"), vote.name);
-	}
-	
-	@tested_feature("Manage votes")
-	@tested_operation("show vote")
-	@tested_behaviour("returns the vote in json")
-	@Test
-	public void check_canAddIn() throws ReportedException {
-		JSONObject result = voteManager.showVote(voteId, adminKey);
-		
-		assertEquals(result.get("canAddIn"), vote.canAddin);
-	}
-
-	@tested_feature("Manage votes")
-	@tested_operation("show vote")
-	@tested_behaviour("returns the vote in json")
-	@Test
-	public void check_creationTime() throws ReportedException {
-		JSONObject result = voteManager.showVote(voteId, adminKey);
-		
-		assertEquals(result.get("creationTime"), vote.creationTime);
-	}
-	
-	@tested_feature("Manage votes")
-	@tested_operation("show vote")
-	@tested_behaviour("returns the vote in json")
-	@Test
-	public void check_choices() throws ReportedException {
-		JSONObject result = voteManager.showVote(voteId, adminKey);
-		JSONObject jsonObject = new JSONObject(vote.choices);
-
-		assertEquals(result.get("choices").toString(), jsonObject.toString());
-	}
-	
-	@tested_feature("Manage votes")
-	@tested_operation("show vote")
-	@tested_behaviour("returns the vote in json")
-	@Test
-	public void check_canEndorse() throws ReportedException {
-		JSONObject result = voteManager.showVote(voteId, adminKey);
-		
-		assertEquals(result.get("canEndorse"), vote.canEndorse);
-	}
-	
-	@tested_feature("Manage votes")
-	@tested_operation("show vote")
-	@tested_behaviour("returns the vote in json")
-	@Test
-	public void check_countedAssurances() throws ReportedException {
-		JSONObject result = voteManager.showVote(voteId, adminKey);
-		JSONArray jsonArray = new JSONArray(vote.countedAssurances);
-		
-		assertEquals(result.get("countedAssurances").toString(), jsonArray.toString());
-	}
-	
-	@tested_feature("Manage votes")
-	@tested_operation("show vote")
-	@tested_behaviour("returns the vote in json")
-	@Test
-	public void check_neededAssurances() throws ReportedException {
-		JSONObject result = voteManager.showVote(voteId, adminKey);
-		JSONArray jsonArray = new JSONArray(vote.neededAssurances);
-		
-		
-		assertEquals(result.get("neededAssurances").toString(), jsonArray.toString());
-	}
-	
-	@tested_feature("Manage votes")
-	@tested_operation("show vote")
-	@tested_behaviour("returns the vote in json")
-	@Test
-	public void check_minEndorsements() throws ReportedException {
-		JSONObject result = voteManager.showVote(voteId, adminKey);
-		
-		assertEquals(result.get("minEndorsements"), vote.minEndorsements);
-	}
-	
-	@tested_feature("Manage votes")
-	@tested_operation("show vote")
-	@tested_behaviour("returns the vote in json")
-	@Test
-	public void check_id() throws ReportedException {
-		JSONObject result = voteManager.showVote(voteId, adminKey);
-		
-		assertEquals(result.get("id"), vote.id);
-	}
-	
-	@tested_feature("Manage votes")
-	@tested_operation("show vote")
-	@tested_behaviour("returns the vote in json")
-	@Test
-	public void check_canView() throws ReportedException {
-		JSONObject result = voteManager.showVote(voteId, adminKey);
-		
-		assertEquals(result.get("canView"), vote.canView);
-	}
-	
-	@tested_feature("Manage votes")
-	@tested_operation("show vote")
-	@tested_behaviour("returns the vote in json")
-	@Test
-	public void check_canVote() throws ReportedException {
-		JSONObject result = voteManager.showVote(voteId, adminKey);
-		
-		assertEquals(result.get("canVote"), vote.canVote);
-	}
-	
 }
