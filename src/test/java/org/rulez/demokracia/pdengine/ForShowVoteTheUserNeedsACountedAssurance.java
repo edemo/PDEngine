db2a659b694ae75fe8fa0e8b4f69d4d246065454
lbarjak <barjak.laszlo@gmail.com>
2019-03-17 10:11:53 +0100

    for_show_vote_the_user_needs_a_counted_assurance_40

---
+++ src/test/java/org/rulez/demokracia/pdengine/ForShowVoteTheUserNeedsACountedAssurance.java
@@ -0,0 +1,38 @@
+package org.rulez.demokracia.pdengine;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.rulez.demokracia.pdengine.annotations.tested_behaviour;
+import org.rulez.demokracia.pdengine.annotations.tested_feature;
+import org.rulez.demokracia.pdengine.annotations.tested_operation;
+import org.rulez.demokracia.pdengine.dataobjects.VoteEntity;
+import org.rulez.demokracia.pdengine.exception.ReportedException;
+import org.rulez.demokracia.pdengine.testhelpers.CreatedDefaultVoteRegistry;
+
+public class ForShowVoteTheUserNeedsACountedAssurance extends CreatedDefaultVoteRegistry {
+	
+	public ForShowVoteTheUserNeedsACountedAssurance() {
+		super();
+	}
+
+	@Test
+	@tested_feature("Manage votes")
+	@tested_operation("show vote")
+	@tested_behaviour("if adminKey is anon, the user should have any of the countedAssurances")
+	public void create_creates_a_vote_with_countedAssurances() throws ReportedException {
+		
+		Vote vote = getTheVote();
+		vote.adminKey = "anon";
+		vote.countedAssurances.add("magyar");
+		if(!(vote.adminKey.equals("anon") 
+				&& vote.neededAssurances.containsAll(countedAssurances))) {
+			fail("anon admin key without countedAssurances");
+		}	
+	}
+}
