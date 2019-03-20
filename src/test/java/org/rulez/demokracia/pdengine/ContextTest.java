d4a28ee476dab3b1df52cc4bc2a4ac8047702abe
Tamas Szirbucz <szirbucz.tamas@gmail.com>
2019-03-20 20:20:04 +0100

    Add test case for obtain context parameters from VoteManager #237

---
+++ src/test/java/org/rulez/demokracia/pdengine/ContextTest.java
@@ -0,0 +1,37 @@
+package org.rulez.demokracia.pdengine;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import javax.xml.ws.WebServiceContext;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.rulez.demokracia.pdengine.annotations.tested_behaviour;
+import org.rulez.demokracia.pdengine.annotations.tested_feature;
+import org.rulez.demokracia.pdengine.annotations.tested_operation;
+import org.rulez.demokracia.pdengine.exception.ReportedException;
+import org.rulez.demokracia.pdengine.testhelpers.CreatedDefaultVoteRegistry;
+
+public class ContextTest extends CreatedDefaultVoteRegistry {
+
+
+	@Before
+	public void setUp() throws ReportedException {
+		super.setUp();
+	}
+	
+	@tested_feature("Supporting functionality")
+	@tested_operation("get context parameter")
+	@tested_behaviour("context parameters can be obtained from the VoteManager")
+	@Test
+	public void testContextParameterCanBeObtainedFromVoteManager() {
+		WebServiceContext wsContext = mock(WebServiceContext.class);
+		VoteManager voteManager = new VoteManager(wsContext);
+		WebServiceContext wsContextFromVoteManager = voteManager.getWsContext();
+		assertEquals(wsContextFromVoteManager, wsContext);
+	}
+
+}
