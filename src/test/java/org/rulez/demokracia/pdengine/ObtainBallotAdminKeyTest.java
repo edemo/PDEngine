7ead43e691d4879c3037c4cdd310775e7428cd52
colorimeter <48435057+colorimeter@users.noreply.github.com>
2019-03-20 13:13:53 +0100

    Feature/not logged in user cannot have ballot 196 (#272)
    * Test if anon admin can only have one ballot #193
    
    * fix tested_behaviour() 193
    
    * Assume 'anon' means 'user' as adminkey'
    
    * fix
    
    * unauthenticated anon admin cannot issue any ballot #196
    
    * polish #196
    
    * fix: check obtained ballot of session user

--- src/test/java/org/rulez/demokracia/pdengine/ObtainBallotAdminKeyTest.java
+++ src/test/java/org/rulez/demokracia/pdengine/ObtainBallotAdminKeyTest.java
@@ -30,7 +30,7 @@ public class ObtainBallotAdminKeyTest extends CreatedDefaultChoice{
 				() -> voteManager.obtainBallot(adminInfo.voteId, "user")
 			).assertMessageIs("The user does not have all of the needed assurances.");
 	}
-	
+
 	@tested_feature("Manage votes")
 	@tested_operation("Obtain ballot")
 	@tested_behaviour("if adminKey is anon, the user should have all the neededAssurances")
@@ -43,7 +43,7 @@ public class ObtainBallotAdminKeyTest extends CreatedDefaultChoice{
 		String ballot = voteManager.obtainBallot(adminInfo.voteId, "user");
 		assertTrue(ballot instanceof String);
 	}
-	
+
 	@tested_feature("Manage votes")
 	@tested_operation("Obtain ballot")
 	@tested_behaviour("if adminKey is anon, the user should have all the neededAssurances")
@@ -54,5 +54,56 @@ public class ObtainBallotAdminKeyTest extends CreatedDefaultChoice{
 		
 		String ballot = voteManager.obtainBallot(adminInfo.voteId, "user");
 		assertTrue(ballot instanceof String);
+	}
+
+	@tested_feature("Manage votes")
+	@tested_operation("Obtain ballot")
+	@tested_behaviour("if adminkey is anon, only one ballot can be issued")
+	@Test
+	public void even_if_the_user_does_have_all_the_assurances_he_cannot_issue_more_than_one_ballot() {
+		Vote vote = voteManager.getVote(adminInfo.voteId);
+		vote.neededAssurances.clear();
+		vote.neededAssurances.add("magyar");
+
+		assertEquals(vote.getRecordedBallots("test_user_in_ws_context").intValue(), 0);
+		String ballot = voteManager.obtainBallot(adminInfo.voteId, "user");
+		assertNotEquals("", ballot );
+		assertEquals(vote.getRecordedBallots("test_user_in_ws_context").intValue(), 1);
+		assertThrows(
+				() -> voteManager.obtainBallot(adminInfo.voteId, "user")
+			).assertMessageIs("This user already have a ballot.");	
+	}
+
+	@tested_feature("Manage votes")
+	@tested_operation("Obtain ballot")
+	@tested_behaviour("Admin can obtain more ballots")
+	@Test
+	public void admin_can_obtain_more_ballots() {
+		Vote vote = voteManager.getVote(adminInfo.voteId);
+		vote.neededAssurances.clear();
+		vote.neededAssurances.add("magyar");
+		
+		String ballot_admin1 = voteManager.obtainBallot(adminInfo.voteId, adminInfo.adminKey);
+		assertNotEquals("", ballot_admin1 );
+		String ballot_admin2 = voteManager.obtainBallot(adminInfo.voteId, adminInfo.adminKey);
+		assertNotEquals("", ballot_admin2 );
+		String ballot_user = voteManager.obtainBallot(adminInfo.voteId, "user");
+		assertNotEquals("", ballot_user );
+	}
+
+	@tested_feature("Manage votes")
+	@tested_operation("Obtain ballot")
+	@tested_behaviour("if the adminKey is anon and the user is not logged in then no ballots are issued")
+	@Test
+	public void not_logged_in_user_cannot_issue_any_ballot() {
+
+		setupUnauthenticatedMockWsContext();
+		Vote vote = voteManager.getVote(adminInfo.voteId);
+		vote.neededAssurances.clear();
+		vote.neededAssurances.add("magyar");
+
+		assertThrows(
+				() -> voteManager.obtainBallot(adminInfo.voteId, "user")
+			).assertMessageIs("Anon admin is not authenticated, cannot issue any ballot.");	
 	}
 }
