8acae536fef5357ba14a7a152bb2ca24c8aabb8d
Tamas Szirbucz <szirbucz.tamas@gmail.com>
2019-03-25 08:30:34 +0100

    Refactored vote filter to eliminate multiple return.

--- src/main/java/org/rulez/demokracia/pdengine/VoteFilter.java
+++ src/main/java/org/rulez/demokracia/pdengine/VoteFilter.java
@@ -1,17 +1,19 @@
 package org.rulez.demokracia.pdengine;
 
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public interface VoteFilter {
 
 	public default List<CastVote> filterVotes(final List<CastVote> votes, final String assurance) {
-		if (Objects.isNull(assurance))
-			return votes;
-
-		return votes.stream()
-				.filter(v->v.getAssurances().contains(assurance))
+		Function<? super String, List<CastVote>> voteFilter = a -> votes.stream()
+				.filter(v -> v.getAssurances().contains(a))
 				.collect(Collectors.toList());
+
+		return Optional.ofNullable(assurance)
+				.map(voteFilter)
+				.orElse(votes);
 	}
 }
