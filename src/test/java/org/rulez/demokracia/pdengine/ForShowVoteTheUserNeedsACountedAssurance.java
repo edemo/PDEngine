192957af79bf93f74150d930a0a943e0ba989fd1
lbarjak <barjak.laszlo@gmail.com>
2019-03-26 23:41:55 +0100

    a_user_with_all_assourances_can_show_the_vote

--- src/test/java/org/rulez/demokracia/pdengine/ForShowVoteTheUserNeedsACountedAssurance.java
+++ src/test/java/org/rulez/demokracia/pdengine/ForShowVoteTheUserNeedsACountedAssurance.java
@@ -1,22 +1,22 @@
 package org.rulez.demokracia.pdengine;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.json.JSONObject;
 import org.junit.Test;
 import org.rulez.demokracia.pdengine.annotations.TestedBehaviour;
 import org.rulez.demokracia.pdengine.annotations.TestedFeature;
 import org.rulez.demokracia.pdengine.annotations.TestedOperation;
+import org.rulez.demokracia.pdengine.dataobjects.VoteAdminInfo;
 import org.rulez.demokracia.pdengine.exception.ReportedException;
 import org.rulez.demokracia.pdengine.testhelpers.CreatedDefaultVoteRegistry;
 
+@TestedFeature("Manage votes")
+@TestedOperation("show vote")
+@TestedBehaviour("if adminKey is anon, the user should have any of the countedAssurances")
 public class ForShowVoteTheUserNeedsACountedAssurance extends CreatedDefaultVoteRegistry {
-	
-	public ForShowVoteTheUserNeedsACountedAssurance() {
-		super();
-	}
 
 	@Test
-	@TestedFeature("Manage votes")
-	@TestedOperation("show vote")
-	@TestedBehaviour("if adminKey is anon, the user should have any of the countedAssurances")
 	public void a_user_with_not_all_assourances_cannot_show_the_vote() throws ReportedException {
 	
 		Vote vote = getTheVote();
@@ -26,9 +26,6 @@ public class ForShowVoteTheUserNeedsACountedAssurance extends CreatedDefaultVote
 	}
 	
 	@Test
-	@TestedFeature("Manage votes")
-	@TestedOperation("show vote")
-	@TestedBehaviour("if adminKey is anon, the user should have any of the countedAssurances")
 	public void a_user_with_not_all_assourances_cannot_show_the_vote_even_with_more_assurances() throws ReportedException {
 	
 		Vote vote = getTheVote();
@@ -37,10 +34,23 @@ public class ForShowVoteTheUserNeedsACountedAssurance extends CreatedDefaultVote
 
 		assertAssurancesMissing(vote);
 	}
+	
+	@Test
+	public void a_user_with_all_assourances_can_show_the_vote() throws ReportedException {
+	
+		Vote vote = getTheVote();
+		vote.countedAssurances.add("magyar");
+		VoteAdminInfo aminInfo = new VoteAdminInfo(vote.id, "user");
+		JSONObject voteJson = voteManager.showVote(aminInfo);
+		
+		assertNotNull(voteJson);
+	}
 
-	private void assertAssurancesMissing(Vote vote) {
-//		assertThrows(
-//				() -> voteManager.showVote(vote.id, "user")
-//			).assertMessageIs("missing assurances");
+	private void assertAssurancesMissing(final Vote vote) {
+		VoteAdminInfo aminInfo = new VoteAdminInfo(vote.id, "user");
+		
+		assertThrows(
+				() -> voteManager.showVote(aminInfo)
+			).assertMessageIs("missing assurances");
 	}
 }
