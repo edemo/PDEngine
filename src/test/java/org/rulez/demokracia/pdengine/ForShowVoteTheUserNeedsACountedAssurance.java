111b53130c6eab61e84ee5a11c016f597fba8a68
lbarjak <barjak.laszlo@gmail.com>
2019-03-26 20:44:03 +0100

    for_show_vote_the_user_needs_a_counted_assurance_40

--- src/test/java/org/rulez/demokracia/pdengine/ForShowVoteTheUserNeedsACountedAssurance.java
+++ src/test/java/org/rulez/demokracia/pdengine/ForShowVoteTheUserNeedsACountedAssurance.java
@@ -1,27 +1,46 @@
 package org.rulez.demokracia.pdengine;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 import org.rulez.demokracia.pdengine.annotations.TestedBehaviour;
 import org.rulez.demokracia.pdengine.annotations.TestedFeature;
 import org.rulez.demokracia.pdengine.annotations.TestedOperation;
+import org.rulez.demokracia.pdengine.exception.ReportedException;
 import org.rulez.demokracia.pdengine.testhelpers.CreatedDefaultVoteRegistry;
 
 public class ForShowVoteTheUserNeedsACountedAssurance extends CreatedDefaultVoteRegistry {
+	
+	public ForShowVoteTheUserNeedsACountedAssurance() {
+		super();
+	}
+
+	@Test
+	@TestedFeature("Manage votes")
+	@TestedOperation("show vote")
+	@TestedBehaviour("if adminKey is anon, the user should have any of the countedAssurances")
+	public void a_user_with_not_all_assourances_cannot_show_the_vote() throws ReportedException {
+	
+		Vote vote = getTheVote();
+		vote.countedAssurances.add("german");
+
+		assertAssurancesMissing(vote);
+	}
 	
 	@Test
 	@TestedFeature("Manage votes")
 	@TestedOperation("show vote")
 	@TestedBehaviour("if adminKey is anon, the user should have any of the countedAssurances")
-	public void create_creates_a_vote_with_countedAssurances() {
-		
+	public void a_user_with_not_all_assourances_cannot_show_the_vote_even_with_more_assurances() throws ReportedException {
+	
 		Vote vote = getTheVote();
-		vote.adminKey = "user";
-		vote.countedAssurances.add(ASSURANCE_NAME);
-		if(!("user".equals(vote.adminKey) 
-				&& vote.neededAssurances.containsAll(countedAssurances))) {
-			fail("anon admin key without countedAssurances");
-		}	
+		vote.countedAssurances.add("magyar");
+		vote.countedAssurances.add("german");
+
+		assertAssurancesMissing(vote);
+	}
+
+	private void assertAssurancesMissing(Vote vote) {
+//		assertThrows(
+//				() -> voteManager.showVote(vote.id, "user")
+//			).assertMessageIs("missing assurances");
 	}
 }
