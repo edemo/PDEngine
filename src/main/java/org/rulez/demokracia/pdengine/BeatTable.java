aa932fab6daa95cf78fe055519ab283769c6c9dc
szirbucz <szirbucz.tamas@gmail.com>
2019-03-30 20:01:32 +0100

    Compute matrix diagonal on normalization #252 (#304)
    * Compute matrix diagonal on normalization #252

--- src/main/java/org/rulez/demokracia/pdengine/BeatTable.java
+++ src/main/java/org/rulez/demokracia/pdengine/BeatTable.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 import org.rulez.demokracia.pdengine.dataobjects.Pair;
 import org.rulez.demokracia.types.MapMatrix;
 
-public class BeatTable extends MapMatrix<String, Pair> implements TransitiveClosable {
+public class BeatTable extends MapMatrix<String, Pair> implements TransitiveClosable, Normalizable {
 	private static final long serialVersionUID = 1L;
 
 	public enum Direction {
