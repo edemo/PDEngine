8886beab97422636d70e808537040dc0c057053a
Tamas Szirbucz <szirbucz.tamas@gmail.com>
2019-03-31 23:32:10 +0200

    Compute vote results stores beatpath matrix #204

--- src/main/java/org/rulez/demokracia/pdengine/BeatTable.java
+++ src/main/java/org/rulez/demokracia/pdengine/BeatTable.java
@@ -2,12 +2,25 @@ package org.rulez.demokracia.pdengine;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 import org.rulez.demokracia.pdengine.dataobjects.Pair;
 import org.rulez.demokracia.types.MapMatrix;
 
 public class BeatTable extends MapMatrix<String, Pair> implements TransitiveClosable, Normalizable {
 	private static final long serialVersionUID = 1L;
+
+	public BeatTable(final BeatTable beatTable) {
+		super(beatTable.getKeyCollection());
+		for (String row : getKeyCollection()) {
+			for (String col : getKeyCollection()) {
+				Pair sourcePair = beatTable.getElement(col, row);
+				if (Objects.nonNull(sourcePair)) {
+					this.setElement(col, row, new Pair(sourcePair.winning, sourcePair.losing));
+				}
+			}
+		}
+	}
 
 	public enum Direction {
 		DIRECTION_FORWARD, DIRECTION_BACKWARD
