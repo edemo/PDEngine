04f8eb1b623ada51c8a26e42026e97e8155cee8b
Bujdosó Valentin <valentinbujdoso@gmail.com>
2019-03-31 21:08:06 +0200

    Feature/normalization of beattable 253 (#306)
    * Normalization of beatTable #253 #254 #255 #256

--- src/main/java/org/rulez/demokracia/pdengine/Normalizable.java
+++ src/main/java/org/rulez/demokracia/pdengine/Normalizable.java
@@ -1,4 +1,8 @@
 package org.rulez.demokracia.pdengine;
+
+import java.util.Collection;
+
+import org.rulez.demokracia.pdengine.BeatTable.Direction;
 import org.rulez.demokracia.pdengine.dataobjects.Pair;
 
 public interface Normalizable extends ContainingBeats {
@@ -8,6 +12,27 @@ public interface Normalizable extends ContainingBeats {
 	default void normalize() {
 		for (String key : getKeyCollection()) {
 			setElement(key, key, new Pair(0, 0));
+		}
+
+		Collection<String> keys = getKeyCollection();
+
+		for (String key1 : keys) {
+			for (String key2 : keys) {
+				int key1Win = beatInformation(key1, key2, Direction.DIRECTION_FORWARD);
+				int key2Win = beatInformation(key2, key1, Direction.DIRECTION_FORWARD);
+
+				if (key1Win > key2Win)
+					setElement(key2, key1, new Pair(0, 0));
+				if (key1Win == key2Win) {
+					int key1Los = beatInformation(key1, key2, Direction.DIRECTION_BACKWARD);
+					int key2Los = beatInformation(key2, key1, Direction.DIRECTION_BACKWARD);
+
+					if (key1Los == key2Los) {
+						setElement(key2, key1, new Pair(0, 0));
+						setElement(key1, key2, new Pair(0, 0));
+					}
+				}
+			}
 		}
 	}
 
