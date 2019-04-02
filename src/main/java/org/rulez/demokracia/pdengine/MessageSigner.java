930082a4f35508babe134dd58287e0607c5287b0
Colorimeter <rexendor@gmail.com>
2019-04-02 10:03:48 +0200

    revert using env variables

--- src/main/java/org/rulez/demokracia/pdengine/MessageSigner.java
+++ src/main/java/org/rulez/demokracia/pdengine/MessageSigner.java
@@ -27,7 +27,9 @@ public class MessageSigner {
 
 	private PublicKey pubkey;
 	private PrivateKey privkey;
+	private static final String KEYSTOREPW="changit";
 	private static final String KEYALIAS="PDEngineKeys";
+	private static final String KEYSTOREPATHINTESTENV="/keystore/keystore.pk12";
 
     private static class Storage {
         private static final MessageSigner INSTANCE = new MessageSigner();
@@ -42,7 +44,7 @@ public class MessageSigner {
 			Context xmlNode = (Context) context.lookup("java:comp/env");
 			keyStorePath = (String) xmlNode.lookup("keyStorePath");
 		} catch (NamingException e2) {
-			keyStorePath= System.getenv("KEYSTORE");
+			keyStorePath= System.getenv("HOME") + KEYSTOREPATHINTESTENV;
 		}
 
 		try {
@@ -51,7 +53,7 @@ public class MessageSigner {
 			throw new ReportedException("Cannot handle PKCS12 keystore");
 		}
 
-    	char[] keyStorePassword = System.getenv("KEYSTOREPASS").toCharArray();
+    	char[] keyStorePassword = KEYSTOREPW.toCharArray();
     	try (InputStream keyStoreData = Files.newInputStream(Paths.get(keyStorePath))) {
 
 	    	keyStore.load(keyStoreData, keyStorePassword);
