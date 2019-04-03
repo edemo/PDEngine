440fac0dfe6e86f1ec81e91678bfbf54a7547b85
Valentin Bujdoso <valentinbujdoso@gmail.com>
2019-04-03 10:11:02 +0200

    Sonar fix

--- src/test/java/org/rulez/demokracia/pdengine/ComputedVoteTest.java
+++ src/test/java/org/rulez/demokracia/pdengine/ComputedVoteTest.java
@@ -1,10 +1,6 @@
 package org.rulez.demokracia.pdengine;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
