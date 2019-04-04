2c7fa16a8dafb16cd4be0dd1c06bc7d2af870d65
szirbucz <szirbucz.tamas@gmail.com>
2019-04-04 16:36:00 +0200

    Non-beaten candidates are winners #246 (#314)
    * Non-beaten andidates are winners #246

--- src/main/java/org/rulez/demokracia/pdengine/Normalizable.java
+++ src/main/java/org/rulez/demokracia/pdengine/Normalizable.java
@@ -10,16 +10,16 @@ public interface Normalizable extends ContainingBeats {
 
 	default void normalize() {
 		Collection<String> keys = getKeyCollection();
-		
+
 		for (String key1 : keys) {
 			for (String key2 : keys) {
 				Pair beats1 = getPair(key1, key2);
 				Pair beats2 = getPair(key2, key1);
 				Pair pair = compareBeats(beats1, beats2);
-				
-				if(pair.equals(beats1))
+
+				if (pair.equals(beats1))
 					setElement(key2, key1, new Pair(0, 0));
-				if(pair.equals(beats2))
+				if (pair.equals(beats2))
 					setElement(key1, key2, new Pair(0, 0));
 			}
 		}
