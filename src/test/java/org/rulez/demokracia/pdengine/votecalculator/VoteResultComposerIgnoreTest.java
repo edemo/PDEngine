e17f0732531732bd6224357b633988ee072882ae
Tamas Szirbucz <szirbucz.tamas@gmail.com>
2019-04-30 10:23:23 +0200

    Add missing test cases to rank candidate #257

---
+++ src/test/java/org/rulez/demokracia/pdengine/votecalculator/VoteResultComposerIgnoreTest.java
@@ -0,0 +1,63 @@
+package org.rulez.demokracia.pdengine.votecalculator;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.rulez.demokracia.pdengine.testhelpers.BeatTableTestHelper.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.rulez.demokracia.pdengine.annotations.TestedBehaviour;
+import org.rulez.demokracia.pdengine.annotations.TestedFeature;
+import org.rulez.demokracia.pdengine.annotations.TestedOperation;
+import org.rulez.demokracia.pdengine.beattable.BeatTable;
+
+@TestedFeature("Schulze method")
+@TestedOperation("rank candidates")
+@TestedBehaviour("calculates winners until all choices are ignored")
+@RunWith(MockitoJUnitRunner.class)
+public class VoteResultComposerIgnoreTest {
+
+  @InjectMocks
+  private VoteResultComposerImpl voteResultComposer;
+  @Mock
+  private WinnerCalculatorService winnerCalculatorService;
+
+  @Test
+  public void
+      winner_calculation_ends_in_one_step_when_all_choices_ignored_at_once() {
+    when(winnerCalculatorService.calculateWinners(any(), any()))
+        .thenReturn(List.of(CHOICE1, CHOICE2, CHOICE3));
+
+    voteResultComposer
+        .composeResult(createNewBeatTableWithComplexData());
+
+    verify(winnerCalculatorService, times(1)).calculateWinners(any(), any());
+  }
+
+  @Test
+  public void
+      winner_calculation_ends_in_n_steps_when_choices_ignored_one_by_one() {
+    when(winnerCalculatorService.calculateWinners(any(), any()))
+        .thenReturn(List.of(CHOICE1))
+        .thenReturn(List.of(CHOICE2))
+        .thenReturn(List.of(CHOICE3));
+
+    voteResultComposer
+        .composeResult(createNewBeatTableWithComplexData());
+
+    verify(winnerCalculatorService, times(3)).calculateWinners(any(), any());
+  }
+
+  @Test
+  public void winner_calculation_ends_in_zero_step_when_beat_table_is_empty() {
+    voteResultComposer
+        .composeResult(new BeatTable());
+
+    verify(winnerCalculatorService, never()).calculateWinners(any(), any());
+  }
+}
